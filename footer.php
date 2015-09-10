cd6b5932cef0107e37683a67079ebfcdfce37c90
Ben <benjamincmarks@gmail.com>
2015-09-09 20:40:59 -0400

    Theme Starter
    Added customized _s base theme

---
+++ footer.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * The template for displaying the footer.
+ *
+ * Contains the closing of the #content div and all content after.
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
+ *
+ * @package flathatnews
+ */
+
+?>
+
+	</div><!-- #content -->
+
+	<footer id="colophon" class="site-footer" role="contentinfo">
+		<div class="site-info">
+			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'fhnews' ) ); ?>"><?php printf( esc_html__( 'Proudly powered by %s', 'fhnews' ), 'WordPress' ); ?></a>
+			<span class="sep"> | </span>
+			<?php printf( esc_html__( 'Theme: %1$s by %2$s.', 'fhnews' ), 'fhnews', '<a href="https://github.com/benjamin-marks" rel="designer">Benjamin Marks</a>' ); ?>
+		</div><!-- .site-info -->
+	</footer><!-- #colophon -->
+</div><!-- #page -->
+
+<?php wp_footer(); ?>
+
+</body>
+</html>
