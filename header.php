a1daad8155b17eb1c2947a25b26ab8d549ed326e
Ben <benjamincmarks@gmail.com>
2015-09-20 13:02:29 -0400

    Test Commit

--- header.php
+++ header.php
@@ -22,6 +22,7 @@
 </head>
 
 <body <?php body_class(); ?>>
+<h1>TEST TEXT</h1>
 <div id="page" class="hfeed site">
 	<a class="skip-link screen-reader-text" href="#content"><?php esc_html_e( 'Skip to content', 'fhnews' ); ?></a>
 
