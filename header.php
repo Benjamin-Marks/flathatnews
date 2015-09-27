d1b5de5ce6cd34183020158dd87b5df425417617
Ann Gao <aygao@email.wm.edu>
2015-09-26 23:29:58 -0400

    Revert "Test Image"
    This reverts commit e70b618490cfd546114819f11916086e5633c5e8.

--- header.php
+++ header.php
@@ -29,7 +29,6 @@
 	<header id="masthead" class="site-header" role="banner">
 		<div class="site-branding">
 			<?php if ( is_front_page() && is_home() ) : ?>
-				<img src="<?php echo $https://runningbreezy.files.wordpress.com/2013/12/sherlock.jpeg;?>">
 				<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
 			<?php else : ?>
 				<p class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></p>
