802067a6fe236495f4cf8d4b2779e86fa23c6b0a
Ben <benjamincmarks@gmail.com>
2015-09-27 12:20:45 -0400

    Parsing Fix, Menu shell start

--- header.php
+++ header.php
@@ -29,7 +29,7 @@
 
 	<header id="masthead" class="site-header" role="banner">
 		<div class="site-branding">
-			<img src="<?php echo http://wmpeople.wm.edu/img/theme3/theme3.jpg?>">
+			<img src=<?php echo "http://wmpeople.wm.edu/img/theme3/theme3.jpg"?>>
 			<?php if ( is_front_page() && is_home() ) : ?>
 				<h2>Freedom of the press!</h2>
 				<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
@@ -41,8 +41,20 @@
 		</div><!-- .site-branding -->
 
 		<nav id="site-navigation" class="main-navigation" role="navigation">
+			<!-- MENU INFORMATION WILL GO HERE -->
+			
+
+
+
+			<?php /*
+
+			IGNORE ME 
+
+			
 			<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e( 'Primary Menu', 'fhnews' ); ?></button>
 			<?php wp_nav_menu( array( 'theme_location' => 'primary', 'menu_id' => 'primary-menu' ) ); ?>
+
+			*/?>
 		</nav><!-- #site-navigation -->
 	</header><!-- #masthead -->
 
