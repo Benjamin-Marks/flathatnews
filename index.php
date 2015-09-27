3ce8b1ea384e22375dd48273ce52ef6a9ddca15c
tvamin <tvamin@email.wm.edu>
2015-09-26 21:30:04 -0400

    More Test Text

--- index.php
+++ index.php
@@ -21,6 +21,7 @@ get_header(); ?>
 
 			<?php if ( is_home() && ! is_front_page() ) : ?>
 				<header>
+					<h1> More Test Text </h1>
 					<h1 class="page-title screen-reader-text"><?php single_post_title(); ?></h1>
 				</header>
 			<?php endif; ?>
